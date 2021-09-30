8130ac4dce683e82e93aa30c756a638c4b7f9dc1
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-09-30 21:24:57 +0200

    Update 52bc74d4ac05d0945d00054e.cs

--- 52bc74d4ac05d0945d00054e.cs
+++ 52bc74d4ac05d0945d00054e.cs
@@ -1,12 +1,6 @@
-// in progress
 using System.Text.RegularExpressions;
 class Kata
 {
   public static string FirstNonRepeatingLetter(string s) =>
-    Regex.Match(s, "(.)(?!.*\\1)", RegexOptions.IgnoreCase);
-}using System.Text.RegularExpressions;
-class Kata
-{
-  public static string FirstNonRepeatingLetter(string s) =>
-    Regex.Match(s, "(.)(?!.*\\1)", RegexOptions.IgnoreCase);
+    Regex.Match(s, @"(.)(?<!\1.+)(?!.*\1)", RegexOptions.IgnoreCase).Value;
 }
