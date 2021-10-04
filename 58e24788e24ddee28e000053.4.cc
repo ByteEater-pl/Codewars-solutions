36a7338a6343cc6da9f447ba8edef89d145494c6
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-10-04 08:51:42 +0200

    Create 58e24788e24ddee28e000053.4.cc

---
+++ 58e24788e24ddee28e000053.4.cc
@@ -0,0 +1,29 @@
+#include <regex>
+using namespace std;
+
+static auto getReg(std::unordered_map<std::string, int>& regs, string name) {
+  return isalpha(name[0]) ? regs[name] : stoi(name);
+}
+
+auto assembler(const vector<string>& program) {
+  cout << endl;
+  vector<smatch> P;
+  unordered_map<string, int> R;
+  transform(begin(program), end(program), back_inserter(P), [](auto& s){
+    auto m = *new smatch;
+    regex_match(s, m, regex{"(\\S*) (\\S*) ?(.*)"});
+    return m;
+  });
+  size_t ip{0};
+  while (ip < size(P)) {
+    if (P[ip][1] == "mov") R[P[ip][2]] = getReg(R, P[ip][3]);
+    if (P[ip][1] == "inc") R[P[ip][2]]++;
+    if (P[ip][1] == "dec") R[P[ip][2]]--;
+    if (P[ip][1] == "jnz") {
+      ip += getReg(R, P[ip][2]) ? getReg(R, P[ip][3]) : 1;
+      continue;
+    }
+    ip++;
+  }
+  return R;
+}
