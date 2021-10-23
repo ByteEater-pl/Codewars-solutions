7ae62b53f417aa9fa055b9f3aeb79d93a8038216
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-10-23 03:00:12 +0200

    Create 589cf45835f99b2909000115.1.sql

---
+++ 589cf45835f99b2909000115.1.sql
@@ -0,0 +1,19 @@
+SELECT
+  w "date",
+  c count,
+  CAST(SUM(c) OVER (ORDER BY w) AS INT) total
+FROM (
+  SELECT
+    CAST(
+      SUBSTRING('000' || y FROM CHAR_LENGTH(y)) || '-'
+      || SUBSTRING('0' || m FROM CHAR_LENGTH(m)) || '-'
+      || SUBSTRING('0' || d FROM CHAR_LENGTH(d)) AS DATE) w,
+    COUNT(*) c
+  FROM (
+    SELECT
+      CAST(EXTRACT(YEAR FROM created_at) AS VARCHAR(4)) y,
+      CAST(EXTRACT(MONTH FROM created_at) AS VARCHAR(2)) m,
+      CAST(EXTRACT(DAY FROM created_at) AS VARCHAR(2)) d
+    FROM posts) u
+  GROUP BY y, m, d) v
+ORDER BY "date"
