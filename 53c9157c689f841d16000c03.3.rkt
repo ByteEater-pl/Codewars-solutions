86e913b13b82bcd29a8e56358a1218bc289dfa17
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-10-25 01:55:46 +0200

    Create 53c9157c689f841d16000c03.3.rkt
    failing as too slow

---
+++ 53c9157c689f841d16000c03.3.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(provide totient)
+
+(define (totient n [i 2] [s 0])
+  (define (f m l)
+    (filter (λ (x) (= 0 (modulo m x))) l))
+  (define (go m l)
+    (if (null? l)
+        m
+        (let ([r (f m l)])
+             (go (apply / m 1 r) r))))
+  (if (< n (sqr i))
+      (let*
+        ([ds (f n (sequence->list s))]
+         [k (go n ds)])
+        (foldl
+          (λ (p a) (- a (/ a p)))
+          n
+          `(,@(if (= k 1) '() `(,k)) ,@ds)))
+      (totient
+        n
+        (+ i 1)
+        (if (for/or
+              ([d s])
+              #:break (< i (sqr d))
+              (= 0 (modulo i d)))
+            s
+            (sequence-append s `(,i))))))
