ba269697963c87971569ad8c7af38fcbf3e7498b
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-11-09 03:40:45 +0100

    Create 54d496788776e49e6b00052f.4.rkt

---
+++ 54d496788776e49e6b00052f.4.rkt
@@ -0,0 +1,41 @@
+#lang racket
+(provide sum-of-divided)
+
+(define (sum-of-divided lst)
+  (define ps (mcons 0 '()))
+  (define t ps)
+  (for*/list
+    ([m
+      (let go ([n 2] [l (map abs lst)])
+        (if (null? l)
+            (mcdr ps)
+            (go
+              (+ n 1)
+              (if
+                (for/or
+                  ([p (mcdr ps)])
+                  #:break (< n (sqr p))
+                  (= 0 (modulo n p)))
+                l
+                (begin
+                  (set-mcdr! t (mcons n '()))
+                  (set! t (mcdr t))
+                  (append-map
+                    (λ (x)
+                       (let exhaust ([s x])
+                         (if (= s 1)
+                             '()
+                             ((λ (q)
+                                 (if (integer? q)
+                                     (exhaust q)
+                                     `(,s)))
+                              (/ s n)))))
+                    l))))))]
+     [s `(,(for/fold
+       ([a '(0 #f)])
+       ([x lst])
+       (if (= 0 (modulo x m))
+           `(,(+ (car a) x) #t)
+           a)))]
+     #:when (cadr s))
+    `(,m ,(car s))))
