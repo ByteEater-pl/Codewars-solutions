df9ff7341403903c346fa9d81a72150f60e4532a
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2021-11-21 17:19:44 +0100

    Create 55084d3898b323f0aa000546.1.scala
    cf. https://www.codewars.com/kata/55084d3898b323f0aa000546/discuss#619800e68f11850016d4bddd

---
+++ 55084d3898b323f0aa000546.1.scala
@@ -0,0 +1,17 @@
+object CaesarCipher {
+  def aLetter(c: Char) = 65 to 90 contains c.&(223)
+  def rotate(n: Int)(c: Char) =
+    if (aLetter(c))
+      ((c & 96) + ((c & 31) + (n % 26) + 25) % 26 + 1)
+      .toChar
+    else c
+  def encode(s: String, shift: Int) = {
+    val h = s.find(aLetter).get.toLower
+    val e = h +: (h +: s).map(rotate(shift))
+    e.grouped((e.size / 5.0).ceil.toInt).toList
+  }
+  def decode(s: List[String]) = {
+    val e = s.flatten
+    e.drop(2).map(rotate(e(0) - e(1))).mkString
+  }
+}
