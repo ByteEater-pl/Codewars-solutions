d24cbdfc96f4f3ec7b9020032f86562af8837cac
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2026-02-07 22:34:57 +0100

    Swap summands
    makes the expression more intuitive

---
+++ 5b76a34ff71e5de9db0000f2.4.rs
@@ -0,0 +1,20 @@
+fn solve(arr: &[&str]) -> String {
+    let mut v: Vec<i16> =
+        arr
+        .iter()
+        .map(|s| {
+            s
+            .bytes()
+            .zip([600, 60, 0, 10, 1])
+            .map(|(b, n)| (b & 15) as i16 * n)
+            .sum()            
+        })
+        .collect();
+    v.sort_unstable();
+    let mut m = 1440 + v[0] - v.last().unwrap();
+    for w in v.windows(2) {
+        m = m.max(w[1] - w[0])
+    }
+    m -= 1;
+    format!["{:02}:{:02}", m / 60, m % 60]
+}
