84c98e8a72fde57fc93101bc994b4f6b7ad09772
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2026-02-20 17:06:04 +0100

    Simplify h12 modulo incrementation
    Implement cuckooClock function to calculate chimes based on input time.

---
+++ 656e4602ee72af0017e37e82.2.scala
@@ -0,0 +1,12 @@
+def cuckooClock(inputTime: String, chimes: Int): String =
+  var Array(h, m) = inputTime.split(':').map(_.toInt)
+  var a = if (m > 0) 0 else h
+  m = (m - 1) / 15 * 15
+  while (a < chimes)
+    a += 1
+    m += 15
+    if (m > 59)
+      h = h % 12 + 1
+      a += h - 1
+      m = 0
+  f"$h%02d:$m%02d"
