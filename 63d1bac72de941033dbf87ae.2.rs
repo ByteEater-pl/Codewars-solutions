3405f76865e854cc5f18a2338f47715dab4811c5
Krzysztof Maczyński <38271367+ByteEater-pl@users.noreply.github.com>
2026-03-16 17:45:56 +0100

    Simplify bool handling

---
+++ 63d1bac72de941033dbf87ae.2.rs
@@ -0,0 +1,21 @@
+fn validate_sudoku(sudoku: &[[u8; 9]; 9]) -> bool {
+    fn check(it: impl Iterator<Item = u8>) -> bool {
+        let mut new = [true; 10];
+        for d in it.chain([0]) {
+            let p = new.get_mut(d as usize).unwrap();
+            if *p {
+                *p = false;
+                continue
+            }
+            return false
+        }
+        true
+    }
+    sudoku
+    .iter()
+    .enumerate()
+    .all(|(i, r)|
+        check(r.iter().copied()) &&
+        check(sudoku.iter().map(|x| x[i])) &&
+        check((0 .. 9).map(|x| sudoku[i / 3 * 3 + x / 3][i % 3 * 3 + x % 3])))
+}
